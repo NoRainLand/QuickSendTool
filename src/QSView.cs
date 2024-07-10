735ad3b006c9517446ddae7723d9fdbe6ac0c4b4
NoRain <wuxinrufeng@qq.com>
2024-07-10 11:07:48 +0800

    提交

--- src/QSView.cs
+++ src/QSView.cs
@@ -31,83 +31,86 @@ namespace NoRain
         public static void ShowView()
         {
             InitForm();
-            ChangeForm();
         }
 
         private static void InitForm()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            form = new Form();
-            form.Text = "QuickSendTool";
+            form = new Form
+            {
+                Text = "QuickSendTool",
 
-            form.Icon = new Icon("./img/Q32.ico");
+                Icon = new Icon("./img/Q32.ico"),
 
-            // 设置窗体不能放大缩小且不能最小化
-            form.FormBorderStyle = FormBorderStyle.FixedDialog;
-            form.MinimizeBox = false;
-            form.MaximizeBox = false;
-
-
-
-            label = new Label();
-            label1 = new Label();
-            textBox1 = new TextBox();
-            label2 = new Label();
-            textBox2 = new TextBox();
-            label3 = new Label();
-            textBox3 = new TextBox();
-            button1 = new Button();
-            label4 = new Label();
-            button2 = new Button();
-        }
-
-        private static void ChangeForm()
-        {
-
-            label!.Text = "1，确认服务器配置";
-
-            label1!.Text = "请输入服务器IP：";
-
-            label2!.Text = "请输入服务器端口：";
-
-            label3!.Text = "请输入服务器API：";
-
-            button1!.Text = "保存配置";
-
-            label4!.Text = "2，添加注册表";
-
-            button2!.Text = "确认添加";
+                // 设置窗体不能放大缩小且不能最小化
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                MinimizeBox = false,
+                MaximizeBox = false,
+                Width = 330,
+                Height = 250
+            };
 
 
 
-            label.Location = new System.Drawing.Point(0, 0);
-            label.AutoSize = true;
-            label1.Location = new System.Drawing.Point(20, 30);
-            label1.Width = 120;
-            textBox1!.Location = new System.Drawing.Point(label1.Location.X + label1.Width, label1.Location.Y - 5);
-            textBox1.Width = 160;
+            label = new Label
+            {
+                Text = "1，确认服务器配置",
+                Location = new Point(0, 0),
+                AutoSize = true,
+            };
+            label1 = new Label
+            {
+                Text = "请输入服务器IP：",
+                Location = new Point(20, 30),
+                Width = 120
+            };
+            textBox1 = new TextBox
+            {
+                Location = new Point(label1.Location.X + label1.Width, label1.Location.Y - 5),
+                Width = 160
+            };
+            label2 = new Label
+            {
+                Text = "请输入服务器端口：",
+                Location = new Point(20, 60),
+                Width = 120
+            };
+            textBox2 = new TextBox
+            {
+                Location = new Point(label2.Location.X + label2.Width, label2.Location.Y - 5),
+                Width = 160
+            };
+            label3 = new Label
+            {
+                Text = "请输入服务器API：",
+                Location = new Point(20, 90),
+                Width = 120
+            };
+            textBox3 = new TextBox
+            {
+                Location = new Point(label3.Location.X + label3.Width, label3.Location.Y - 5),
+                Width = 160
+            };
+            button1 = new Button
+            {
+                Text = "保存配置",
+                Location = new Point(20, 120)
+            };
+            label4 = new Label
+            {
+                Text = "2，添加注册表",
+                Location = new Point(0, 150),
+                AutoSize = true
+            };
+            button2 = new Button
+            {
+                Text = "确认添加",
+                Location = new Point(20, 180)
+            };
 
-            label2.Location = new System.Drawing.Point(20, 60);
-            label2.Width = 120;
-            textBox2!.Location = new System.Drawing.Point(label2.Location.X + label2.Width, label2.Location.Y - 5);
-            textBox2.Width = 160;
 
-
-
-            label3.Location = new System.Drawing.Point(20, 90);
-            label3.Width = 120;
-            textBox3!.Location = new System.Drawing.Point(label3.Location.X + label3.Width, label3.Location.Y - 5);
-            textBox3.Width = 160;
-
-            button1.Location = new System.Drawing.Point(20, 120);
-
-            label4.Location = new System.Drawing.Point(0, 150);
-            label4.AutoSize = true;
-            button2.Location = new System.Drawing.Point(20, 180);
-
-
-            form!.Controls.Add(label);
+            form.Controls.Add(label);
             form.Controls.Add(label1);
             form.Controls.Add(textBox1);
             form.Controls.Add(label2);
@@ -117,11 +120,8 @@ namespace NoRain
             form.Controls.Add(button1);
             form.Controls.Add(label4);
             form.Controls.Add(button2);
-
-            form.Width = 330;
-            form.Height = 250;
-
             Application.Run(form);
         }
+
     }
 }
